a7c09284ab1d21242cfd35c70affe91fe48da492
shimulkhan <khanshimul196@gmail.com>
2021-10-31 04:13:15 +0600

    emplement delete one

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function server() {
         // Find single package use id
         app.get('/packages/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) }
+            const query = { _id: ObjectId(id) };
             const package = await packageCollection.findOne(query);
             res.send(package);
         });
@@ -50,6 +50,15 @@ async function server() {
             const cursor = bookedCollection.find({});
             const bookedPackages = await cursor.toArray();
             res.send(bookedPackages);
+        })
+
+        // My Orders DELETE API
+        app.delete('/booked/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookedCollection.deleteOne(query);
+            console.log(result)
+            // res.json(result);
         })
 
         // Add package POST API
